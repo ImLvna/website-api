362f3633698342939ab0d170977944c46296db94
Luna <imlvnaa@gmail.com>
2023-09-10 16:14:17 -0400

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { Router } from 'itty-router';
+import { getData } from './routes/discord';
+
+export interface Env {
+	DISCORD_BOT_TOKEN: string;
+	DISCORD_OWNER_ID: string;
+}
+
+const router = Router();
+
+const resJson = (obj: any) =>
+	new Response(JSON.stringify(obj), { headers: { 'content-type': 'application/json' } });
+
+router.get('/discord', async (request, env: Env) => {
+	return resJson(await getData(env.DISCORD_OWNER_ID, env.DISCORD_BOT_TOKEN));
+});
+
+export default {
+	fetch: router.handle,
+};
